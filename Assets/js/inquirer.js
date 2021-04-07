16dd36ba869ae1a7eaa7a63b537e4474f6fb6b04
Hannah Lee <013hannah.a.lee@gmail.com>
2021-04-06 19:02:31 -0500

    added inquirer messages for manager input

--- Assets/js/inquirer.js
+++ Assets/js/inquirer.js
@@ -3,7 +3,29 @@ const inquirer = require('inquirer');
 function init() {
     inquirer
         .prompt([{
-            type: ''
+            type: 'input',
+            message: `What is the team manager's name?`,
+            name: 'name'
+        },
+        {
+            type:'input',
+            message: `What is the team manager's employee ID?`,
+            name: 'id',
+        },
+        {
+            type: 'input',
+            message: `What is the team manager's email address?`,
+            name: 'email',
+        },
+        {
+            type: 'input',
+            message: `What is the team manager's office number?`,
+            name: 'office',
         }
     ])
+    .then ((response) => {
+        console.log(response)
+    })
 }
+
+init();
