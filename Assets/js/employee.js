447bd79300bfd422b332d74ae5c3159981ea6f7a
Hannah Lee <013hannah.a.lee@gmail.com>
2021-04-06 20:01:39 -0500

    application will now run through all the questions regarding team members. It will take the user back to the menu of employees to add after adding one. If user inputs finished with my team, the questions will end

--- Assets/js/employee.js
+++ Assets/js/employee.js
@@ -1,32 +1,101 @@
 const inquirer = require('inquirer');
 
-function addTeam(data) {
-    if (data.employee !== 'none') {
-        inquirer
-        .prompt([{
-            type: 'list',
-            message: `Please select to add an engineer, an intern, or if you are finished building your team.`,
-            name: 'employee',
-            choices: [
-                {
-                    name: 'Engineer',
-                    value: 'engineer',
-                },
-                {
-                    name: 'Intern',
-                    value: 'intern',
-                },
-                {
-                    name: 'Finished with my team',
-                    value: 'none',
-                }
-            ]
-        }    
-        ])
-        .then ((response) => {
-            console.log(response)
-        })
-    }
+function addTeam() {
+    inquirer
+    .prompt([{
+        type: 'list',
+        message: `Please select to add an engineer, an intern, or if you are finished building your team.`,
+        name: 'employee',
+        choices: [
+            {
+                name: 'Engineer',
+                value: 'engineer',
+            },
+            {
+                name: 'Intern',
+                value: 'intern',
+            },
+            {
+                name: 'Finished with my team',
+                value: 'none',
+            }
+        ]
+    }    
+    ])
+    .then ((response) => {
+        console.log(response)
+        if (response.employee == 'none') {
+            return;
+        }
+
+        if (response.employee == 'engineer') {
+            addEngineer()
+        }
+
+        if (response.employee == 'intern') {
+            addIntern()
+        }
+    })
 }
 
+function addIntern() {
+    inquirer
+    .prompt([{
+        type: 'input',
+        message: `What is the intern's name?`,
+        name: 'name'
+    },
+    {
+        type:'input',
+        message: `What is the intern's employee ID?`,
+        name: 'id',
+    },
+    {
+        type: 'input',
+        message: `What is the intern's email address?`,
+        name: 'email',
+    },
+    {
+        type: 'input',
+        message: `What is the intern's school?`,
+        name: 'school',
+    }
+    ])
+    .then ((response) => {
+    console.log(response);
+    addTeam();
+    })
+}    
+
+function addEngineer() {
+    inquirer
+    .prompt([{
+        type: 'input',
+        message: `What is the engineer's name?`,
+        name: 'name'
+    },
+    {
+        type:'input',
+        message: `What is the engineer's employee ID?`,
+        name: 'id',
+    },
+    {
+        type: 'input',
+        message: `What is the engineer's email address?`,
+        name: 'email',
+    },
+    {
+        type: 'input',
+        message: `What is the engineer's GitHub username?`,
+        name: 'GitHub',
+    }
+    ])
+    .then ((response) => {
+    console.log(response);
+    addTeam();
+    })
+}    
+
 module.exports = addTeam;
+module.exports = addIntern;
+module.exports = addEngineer;
--- Assets/js/inquirer.js
+++ Assets/js/inquirer.js
@@ -24,27 +24,9 @@ function init() {
             message: `What is the team manager's office number?`,
             name: 'office',
         },
-        // {
-        //     type: 'list',
-        //     message: `Please select to add an engineer, an intern, or if you are finished building your team.`,
-        //     name: 'employee',
-        //     choices: [
-        //         {
-        //             name: 'Engineer',
-        //             value: 'engineer',
-        //         },
-        //         {
-        //             name: 'Intern',
-        //             value: 'intern',
-        //         },
-        //         {
-        //             name: 'Finished with my team',
-        //             value: 'none',
-        //         }
-        //     ]
-        // }
     ])
     .then ((response) => {
+        console.log(response);
         addTeam(response);
     })
 }
