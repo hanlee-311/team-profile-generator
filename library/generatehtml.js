3366a6f6a7ee840afb6e1f79b018fd42abf7678b
Hannah Lee <013hannah.a.lee@gmail.com>
2021-05-02 10:23:48 -0500

    app can generate a simple card for the manager based on user input

--- library/generatehtml.js
+++ library/generatehtml.js
@@ -1,7 +1,23 @@
 function generateTeamPage(team) {
     const generateManager = manager => {
         return `
-        <h1>${manager.getEmpName()}<h1>
+        <div class="row">
+        <div class="col s12 m6">
+        <div class="card">
+        <div class="card-content red lighten-3">
+        <span class="card-title" style='font-weight: bold;'>${manager.getEmpName()}</span>
+        <span class="card-title" style='font-weight: bold;'><i class="free_breakfast"></i>${manager.getRole()}</span>
+            <div class="card-content red lighten-5">
+            <ul class="collection">
+                <li class="collection-item">ID: ${manager.getId()}</li>
+                <li class="collection-item">Email: ${manager.getEmail()}</li>
+                <li class="collection-item">Office Number: ${manager.getOfficeNumber()}</li>
+            </ul>
+            </div>
+        </div>
+        </div>
+        </div>
+        </div>
         `
     }
 
@@ -34,11 +50,19 @@ module.exports = team => {
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css">
+    <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
     <title>My Team</title>
     </head>
     <body>
-        <h1>This is just a template html file for me to use. Will be deleted once project is ready.</h1>
-        ${generateTeamPage(team)}
+
+    <nav>
+    <div class="nav-wrapper">
+        <a href="#" class="brand-logo center">My Team</a>
+    </div>
+    </nav>
+
+    ${generateTeamPage(team)}
     </body>
     </html>
     `
